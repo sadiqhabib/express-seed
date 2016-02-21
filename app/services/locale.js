013490bae47aaa6220d54c0d8e840d75c872ee0f
Adam Buczynski <adambuczynski@gmail.com>
2016-02-22 10:25:18 +1300

    Add some comments

--- app/services/locale.js
+++ app/services/locale.js
@@ -9,7 +9,11 @@ let i18n = require('i18n');
  * Create locale bound locale/translation service
  */
 let Locale = function(locale) {
+
+  //Set locale
   this.locale = locale;
+
+  //Expose translation functions
   this.t = i18n.__;
 };
 
