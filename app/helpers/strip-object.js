f2eb1401335603d8059e78cb05325672cca047c5
Adam Buczynski <adambuczynski@gmail.com>
2016-06-05 09:23:56 +1200

    Strip object to better handle ObjectID's

--- app/helpers/strip-object.js
+++ app/helpers/strip-object.js
@@ -1,11 +1,24 @@
 'use strict';
 
 /**
- * Internal helper
+ * Dependencies
  */
-function strip(properties, obj) {
+let chalk = require('chalk');
+let ObjectId = require('mongoose').Types.ObjectId;
+
+/**
+ * Helper to strip an object to only certain properties
+ */
+module.exports = function stripObject(obj, ...properties) {
+  if (Array.isArray(obj)) {
+    return obj.map(obj => stripObject.apply(null, [obj].concat(properties)));
+  }
   if (!obj || typeof obj !== 'object') {
     return obj;
+  }
+  if (obj instanceof ObjectId) {
+    console.warn(chalk.yellow('ObjectID passed to stripObject helper'));
+    return obj.toString();
   }
   let stripped = {};
   properties.forEach(property => {
@@ -14,14 +27,4 @@ function strip(properties, obj) {
     }
   });
   return stripped;
-}
-
-/**
- * Helper to strip an object to only certain properties
- */
-module.exports = function stripObject(obj, ...properties) {
-  if (Array.isArray(obj)) {
-    return obj.map(strip.bind(null, properties));
-  }
-  return strip(properties, obj);
 };
