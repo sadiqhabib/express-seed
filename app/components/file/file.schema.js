ef5242dddcb6c94817c68687cf1f8cbcd4bf4639
Adam Buczynski <me@adambuczynski.com>
2016-07-21 09:17:05 +1200

    Extensible file schema

--- app/components/file/file.schema.js
+++ app/components/file/file.schema.js
@@ -12,26 +12,38 @@ let config = require('../../config');
 const BASE_URL = config.GCLOUD_STORAGE_BASE_URL;
 
 /**
- * Google Cloud Storage file schema
+ * Schema generator
  */
-let FileSchema = new Schema({
-  bucket: String,
-  path: String,
-}, {
-  _id: false,
-});
+function extendFileSchema(fields, _id = false) {
+
+  //Define fields
+  fields = Object.assign({
+    bucket: String,
+    path: String,
+  }, fields || {});
+
+  //Create schema
+  let FileSchema = new Schema(fields, {_id});
+
+  //Add transform
+  FileSchema.options.toJSON = {
+    transform(doc, ret) {
+      if (ret.path && ret.bucket) {
+        return BASE_URL + ret.bucket + '/' + ret.path;
+      }
+      return null;
+    },
+  };
+
+  //Return
+  return FileSchema;
+}
 
 /**
- * Transformation to JSON
+ * Create basic schema and expose creation method
  */
-FileSchema.options.toJSON = {
-  transform(doc, ret) {
-    if (ret.path && ret.bucket) {
-      return BASE_URL + ret.bucket + '/' + ret.path;
-    }
-    return null;
-  },
-};
+let FileSchema = extendFileSchema(null, false);
+FileSchema.extend = extendFileSchema;
 
 /**
  * Export schema
