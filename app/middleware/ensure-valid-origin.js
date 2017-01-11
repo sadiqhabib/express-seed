1e8b231dc5bbe6bd46c3835a2f36b41e98174528
Adam Reis <adam@reis.nz>
2017-01-12 10:56:17 +1300

    Update origin validation

--- app/middleware/ensure-valid-origin.js
+++ app/middleware/ensure-valid-origin.js
@@ -3,6 +3,7 @@
 /**
  * Dependencies
  */
+const url = require('url');
 const errors = require('meanie-express-error-handling');
 const NotAuthorizedError = errors.NotAuthorizedError;
 
@@ -11,9 +12,15 @@ const NotAuthorizedError = errors.NotAuthorizedError;
  */
 module.exports = function ensureValidOrigin(req, res, next) {
 
-  //Get app origins
+  //Check if anything present
+  if (!req.headers.origin && !req.headers.referer) {
+    return next();
+  }
+
+  //Determine origin
+  const ref = url.parse(req.headers.origin || req.headers.referer);
+  const origin = ref.protocol + '//' + ref.host;
   const origins = req.app.locals.APP_ORIGINS;
-  const origin = req.headers.origin || req.headers.referer;
 
   //If nothing there, all good
   if (!Array.isArray(origins) || origins.length === 0) {
@@ -27,8 +34,11 @@ module.exports = function ensureValidOrigin(req, res, next) {
 
   //Check if matches something
   if (!origins.some(pattern => origin.match(pattern))) {
-    return next(new NotAuthorizedError('Invalid origin'));
+    return next(new NotAuthorizedError('Invalid origin: ' + origin));
   }
+
+  //Set domain in locals
+  req.locals.origin = ref.hostname;
 
   //All good
   next();
