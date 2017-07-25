561d94fa35dcb27c9d89f4ebd20f4155182f7f59
Adam Reis <adam@reis.nz>
2017-07-26 11:59:35 +1200

    Move handlebars transforms

--- app/init/handlebars.js
+++ app/init/handlebars.js
@@ -10,16 +10,16 @@ const handlebars = require('handlebars');
 /**
  * Settings
  */
-const PATH_HELPERS = path.resolve('./app/helpers/transform/');
+const PATH_TRANSFORMS = path.resolve('./app/transforms/');
 
 /**
  * Register handlebars helpers
  */
-fs.readdirSync(PATH_HELPERS)
+fs.readdirSync(PATH_TRANSFORMS)
   .forEach(file => {
     const ext = path.extname(file);
     const name = path.basename(file, ext);
-    const helperPath = path.join(PATH_HELPERS, file);
+    const helperPath = path.join(PATH_TRANSFORMS, file);
     const helper = require(helperPath);
     handlebars.registerHelper(name, helper);
   });
